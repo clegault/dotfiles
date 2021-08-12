e3c51e74ccd6d95ae929692e57c4dbbeb936bb6b
Collin LeGault <1413504+clegault@users.noreply.github.com>
2021-08-12 16:27:32 -0700

    fixed the vim install

--- setup.d/08-vim.sh
+++ setup.d/08-vim.sh
@@ -14,13 +14,6 @@ fi
 # want it installed, just uncomment the line below:
 # git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Install Vim Plug.
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # Use our dotfiles for vimrc and vim spell.
 ensure_symlink "$(pwd)/vim/vim-spell-en.utf-8.add" "$HOME/.vim-spell-en.utf-8.add"
 ensure_symlink "$(pwd)/vim/vimrc" "$HOME/.vimrc"
-
-# Install plugins. THIS IS BROKEN
-vim +'PlugInstall --sync' +qa
