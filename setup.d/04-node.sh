e150cbfb8fa3dd1df1326a2f163f00fbb2bbe3b6
Collin LeGault <1413504+clegault@users.noreply.github.com>
2021-09-04 16:03:25 -0700

    Learned how to install native node on m1 mac

--- setup.d/04-node.sh
+++ setup.d/04-node.sh
@@ -1,23 +1,19 @@
-#check if this is an M1 mac, if yes, abort
-if [[ $type == "osx-arm" ]]; then
-    continue
+# If NVM is not installed, install it.
+echo "$os: Checking for NVM..."
+nvm_installed=$(command -v nvm)
+if [[ ${nvm_installed} != 0 ]]; then
+    echo "$os: Installing NVM..."
+    touch ~/.bash_profile
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | zsh
 else
-    # If NVM is not installed, install it.
-    echo "$os: Checking for NVM..."
-    nvm_installed=$(command -v nvm)
-    if [[ ${nvm_installed} != 0 ]]; then
-        echo "$os: Installing NVM..."
-        touch ~/.bash_profile
-        # TODO: make sure this also does zsh
-        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-    else
-        echo "$os: NVM is installed..."
-    fi
-
-    # Install the current node lts.
-    echo "Installing and use node lts as default"
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    nvm install 'lts/*'
-    nvm alias default 'lts/*'
+    echo "$os: NVM is installed..."
 fi
+
+# Install the current node lts.
+echo "Installing and use node lts as default"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" 
+nvm install 'lts/*'
+nvm alias default 'lts/*'
+nvm cache clear
