fb1fa1ba9ecde608f05a9f16f0dd8e64302cfd71
Collin LeGault <1413504+clegault@users.noreply.github.com>
2021-09-05 18:10:30 -0700

    rearranged git installation
    seems we were installing git multiple times. moved git brew install to command line and fixed the initial install script to install git up front. mv'd git install script to gnupg as thats what it is really doing anyway.

--- install.sh
+++ install.sh
@@ -1,11 +1,38 @@
-if [ ! -x "$(command -v git)" ]; then
-    echo "you need to run xcode-select --install first"
-    return 0
+if [[ -x "$(command -v xcode-select)" ]]; then
+    xcode-select --install
+    echo "Follow the prompts on the screen to finish xcode installation. Press any key to continue once the installer is finished."
+    while [ true ] ; do
+        read -t 3 -n 1
+        if [ $? = 0 ] ; then
+            exit ;
+        else
+            echo "waiting for the keypress"
+        fi
+    done
 else
-    echo "executing"
-    mkdir ~/src
-    cd ~/src/
-    git clone https://github.com/clegault/dotfiles.git
-    cd dotfiles
-    ./setup.sh -auto
+    sudo apt update
+    sudo apt upgrade -y
 fi
+if [[ ! -x "$(command -v git)" ]]; then
+    echo "Installing git"
+    sudo apt install -y git
+fi
+# Configure Git.
+if [[ "$USER" == "clegault" ]]; then
+    # if ask "$os: Configure git for clegault user and GPG signing?" n; then
+        git config --global user.name "Collin LeGault"
+        git config --global user.email "1413504+clegault@users.noreply.github.com"
+        git config --global user.signingKey ""
+        git config --global commit.gpgSign false
+        git config --global tag.forceSignAnnotated false
+        git config --global gpg.program "gpg"
+        # Note: on ubuntu we might need:
+        # git config --global gpg.program "gpg2"
+    # fi
+fi
+echo "Starting installation..."
+mkdir ~/src
+cd ~/src/
+git clone https://github.com/clegault/dotfiles.git
+cd dotfiles
+exec ./setup.sh -auto
