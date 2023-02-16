7f8136daf62b90deb9185b4e26c47c7b50301995
Collin LeGault <1413504+clegault@users.noreply.github.com>
2023-02-16 15:33:44 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-if [ -x "$(command -v xcode-select)" ]; then
-    xcode-select --install
-    echo "Follow the prompts on the screen to finish xcode installation. Press any key to continue once the installer is finished."
-    while [ true ] ; do
-        read -t 3 -n 1
-        if [ $? = 0 ] ; then
-            exit ;
-        else
-            echo "waiting for the keypress"
-        fi
-    done
-else
-    sudo apt update
-    sudo apt upgrade -y
-fi
-if [ ! -x "$(command -v git)" ]; then
-    echo "Installing git"
-    sudo apt install -y git
-fi
+#if [ -x "$(command -v xcode-select)" ]; then
+#    xcode-select --install
+#    echo "Follow the prompts on the screen to finish xcode installation. Press any key to continue once the installer is finished."
+#    while [ true ] ; do
+#        read -t 3 -n 1
+#        if [ $? = 0 ] ; then
+#            exit ;
+#        else
+#            echo "waiting for the keypress"
+#        fi
+#    done
+#else
+#    sudo apt update
+#    sudo apt upgrade -y
+#fi
+#if [ ! -x "$(command -v git)" ]; then
+#    echo "Installing git"
+#    sudo apt install -y git
+#fi
 echo "Starting installation..."
 mkdir ~/src
 cd ~/src/
